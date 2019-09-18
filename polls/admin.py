e714691a53d2dd59b805c3d9b80c7110fe2810e9
Abisek <avisekmishra81@ymail.com>
2019-09-18 19:59:58 +0530

    Added Views and Rendering Templates

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 
 # Register your models here.
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'asked_date', 'was_published_recently')
+
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ("Question Information", {"fields": ["asked_date", "description"],
+                                  "classes": ['collapse']})
+    ]
+
+    inlines = [ChoiceInline]
+    list_filter = ['asked_date']
+    search_fields = ['question_text']
+    # fields = ['asked_date', 'question_text', 'description']
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 
 # Create your models here.
@@ -6,6 +8,13 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     asked_date = models.DateTimeField('date asked')
+
+    def was_published_recently(self):
+        return self.asked_date > timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field = 'asked_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published Recently?'
 
     def __unicode__(self):
         return self.question_text
